d5a131928c23261a3aaf390af0982681764fb105
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2023-10-22 17:48:05 +0530

    Add types for root state and app dispatch

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import store from "./store.ts";
+import { store } from "./store.ts";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
