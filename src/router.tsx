82f2947a72125be1ac9e2791287d10075dd79eea
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2023-10-22 18:04:03 +0530

    Misc changes

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store.ts';
 
 export const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <App/>
-	}
-])
+		element: (
+			<Provider store={store}>
+				<App />
+			</Provider>
+		),
+	},
+]);
