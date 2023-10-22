c4dde363b858045fc38dd2d7ed2d10a11706914c
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2023-10-22 18:08:52 +0530

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
 	content: [],
 	theme: {
+		colors: {
+			accent: '#E54065',
+			background: '#F4F5F9',
+			border:'#CFD2DC',
+			text: '#636363',
+			'filter-button': '#E1E4EA',
+			'read-background': '#F2F2F2'
+		},
 		extend: {},
 	},
 	plugins: [],
