eb641f1f65a4c08ace6bd88d1ee14bd260afde84
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2023-10-22 20:11:12 +0530

    Add email reducer

--- src/reducers/email.ts
+++ src/reducers/email.ts
@@ -1,10 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { TEmail, TEmailDetail } from '../types';
 
+interface EmailSlice {
+	emailIds: string[];
+	emails: Record<string, TEmail>;
+	emailDetails: Record<string, TEmailDetail>;
+	totalEmails?: number;
+}
+const initialState: EmailSlice = {
+	emailIds: [],
+	emails: {},
+	emailDetails: {},
+};
 export const emailSlice = createSlice({
 	name: 'email',
-	initialState: {
-		emails: {},
-		emailDetails: {},
+	initialState,
+	reducers: {
+		addEmails: (state, action: PayloadAction<TEmail[]>) => {
+			action.payload.forEach((email) => {
+				const { id } = email;
+				if (state.emails[id]) return;
+				state.emailIds.push(id);
+				state.emails[id] = email;
+			});
+		},
+		updateTotalEmails: (state, action: PayloadAction<number>) => {
+			state.totalEmails = action.payload;
+		},
 	},
-	reducers: {},
 });
+
+export const { addEmails, updateTotalEmails } = emailSlice.actions;
+
+export const selectEmailIds = (state: { email: EmailSlice }) =>
+	state.email.emailIds;
+export const selectEmailById = (state: { email: EmailSlice }, id: string) =>
+	state.email.emails[id];
+
+export const shouldFetchMoreEmails = (state: { email: EmailSlice }) => {
+	if (state.email.totalEmails === undefined) return true;
+	return state.email.emailIds.length < state.email.totalEmails;
+};
+
+export default emailSlice.reducer;
